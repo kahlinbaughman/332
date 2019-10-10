4393a261fe746c702cc352863e73738d93e00c9e
Kahlin Baughman <kahlin@hawaii.edu>
2019-10-09 18:16:47 -1000

    Finish Exercise 1 Q2

--- ics332_hw3_kahlin/kvstore/KVServer2.java
+++ ics332_hw3_kahlin/kvstore/KVServer2.java
@@ -4,16 +4,68 @@ import java.rmi.Naming;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.HashMap;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.InputStream;
 
-public class KVServer extends UnicastRemoteObject implements KVInterface {
+public class KVServer2 extends UnicastRemoteObject implements KVInterface {
 
     private static final String serverName = "rmi://localhost/Server";
 
     private HashMap<String, String> kv = new HashMap<String, String>();
 
+    private static String[] checkReg = ("ps -o pid= -C rmiregistry").split("[ \t\n]+");
+    private static String rmiReg = "rmiregistry";
+
     // Custom constructor that throws the required exception
-    protected KVServer() throws RemoteException {
+    protected KVServer2() throws RemoteException {
         super();
+    }
+
+    private static Boolean checkRmi() {
+
+        Boolean running = false;
+
+        Process p = null;
+        ProcessBuilder pb = new ProcessBuilder(checkReg);
+
+        try
+        {
+            p = pb.start();
+        } catch (Exception e) {
+        }
+
+        InputStream processOut = p.getInputStream();
+        BufferedReader input = new BufferedReader(new InputStreamReader(processOut));
+
+        try {
+            if (input.readLine() == null)
+            {
+                running = false;
+            }
+            else
+            {
+                running = true;
+            }
+        } catch (Exception e) {
+
+        }
+
+        return running;
+    }
+
+    private static void runRmi() {
+
+        Process p = null;
+        ProcessBuilder pb = new ProcessBuilder(rmiReg);
+
+        try
+        {
+            p = pb.start();
+            Thread.sleep(1000);
+        } catch (Exception e) {
+        }
+
     }
 
     @Override
@@ -51,6 +103,14 @@ public class KVServer extends UnicastRemoteObject implements KVInterface {
     }
 
     public static void main(String[] args) {
+
+        if (!checkRmi()) {
+            System.out.println("KVServer: Starting RMI Registry…");
+            runRmi();
+            System.out.println("KVServer: RMI Registry started");
+        } else {
+            System.out.println("KVServer: RMI Registry already running!");
+        }
 
         try {
             // Register myself to the RMI Registry
